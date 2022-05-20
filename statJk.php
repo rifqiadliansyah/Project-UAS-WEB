d4578ff1e79bdbfab83a8e27ee04163fec4731e8
msdzilii <83410300+msdzilii@users.noreply.github.com>
2022-05-20 21:03:22 +0700

    Edit-UI-Statistik

--- statJk.php
+++ statJk.php
@@ -24,24 +24,34 @@ foreach ($queryJk as $q) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <style>
-        .chart-container {
-            position: relative;
-            margin: auto;
-            height: 40vh;
-            width: 40vw;
-        }
-    </style>
+
 </head>
 
 <body>
+    <nav class="navbar navbar-light fs-5 mb-2" style="background-color: #8CC0DE;">
+            <a class="nav-link active text-decoration-none"  href="index.php" style="color: #343434;">Projek Rekap Pasien Hepatitis Akut</a>
+            <a class="nav-search-bar justify-content-right list-style-type none" href="">
 
-    <div align="center">
+                <li class="nav-item dropdown list-unstyled justify-content-right " >
+                    <a class="nav-link dropdown-toggle" style="color: #343434;" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Lihat Statistik Pasien</a>
+                    <ul class="dropdown-menu list-style-type-none">
+                        <li><a class="dropdown-item" href="statAlamat.php">Alamat</a></li>
+                        <li><a class="dropdown-item" href="statKategori.php">Kategori</a></li>
+                        <li><a class="dropdown-item" href="statJk.php">Jenis Kelamin</a></li>
+                    </ul>
+                </li>
+
+            </a>
+    </nav>
+
+    <div class ="layout-main">
         <h1>Statistik Pasien Berdasarkan Jenis Kelamin</h1>
     </div>
-    <div class="chart-container">
+    <div class="chart-container-jk">
         <canvas id="myChart" width="200" height="100"></canvas>
     </div>
 
@@ -80,6 +90,12 @@ foreach ($queryJk as $q) {
             }
         });
     </script>
+
+    <!-- Boostrap bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- font awesome (untuk icon) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </body>
 
 </html>
--- statKategori.php
+++ statKategori.php
@@ -20,23 +20,33 @@ foreach ($queryKategori as $k) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <style>
-        .chart-container {
-            position: relative;
-            margin: auto;
-            height: 40vh;
-            width: 40vw;
-        }
-    </style>
+
 </head>
 
 <body>
-    <div align="center">
+    <nav class="navbar navbar-light fs-5 mb-2" style="background-color: #8CC0DE;">
+            <a class="nav-link active text-decoration-none"  href="index.php" style="color: #343434;">Projek Rekap Pasien Hepatitis Akut</a>
+            <a class="nav-search-bar justify-content-right list-style-type none" href="">
+
+                <li class="nav-item dropdown list-unstyled justify-content-right " >
+                    <a class="nav-link dropdown-toggle" style="color: #343434;" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Lihat Statistik Pasien</a>
+                    <ul class="dropdown-menu list-style-type-none">
+                        <li><a class="dropdown-item" href="statAlamat.php">Alamat</a></li>
+                        <li><a class="dropdown-item" href="statKategori.php">Kategori</a></li>
+                        <li><a class="dropdown-item" href="statJk.php">Jenis Kelamin</a></li>
+                    </ul>
+                </li>
+
+            </a>
+    </nav>
+    <div class="layout-main">
         <h1>Statistik Pasien Berdasarkan Kategori Usia</h1>
-    </div>
-    <div class="chart-container">
+    </div><br><br>
+    <div class="chart-container-usia">
         <canvas id="myChart" width="400" height="100"></canvas>
     </div>
     <script>
@@ -73,6 +83,11 @@ foreach ($queryKategori as $k) {
             }
         });
     </script>
+     <!-- Boostrap bundle -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- font awesome (untuk icon) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </body>
 
 </html>
